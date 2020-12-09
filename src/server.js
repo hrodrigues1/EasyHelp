d2a1fdd8dd1512b66a2a48ecd61319d148755b2e
hrodrigues1 <55166332+hrodrigues1@users.noreply.github.com>
2020-12-09 17:41:13 -0300

    Revert "persistencia"
    This reverts commit ab2d0b9e715746879e246b691f60ac996d7ddf27.

--- src/server.js
+++ src/server.js
@@ -1,23 +1,106 @@
+const proffys = [
+    {   name:"Rodrigo Aguiar", 
+        avatar :"https://avatars2.githubusercontent.com/u/74689679?s=400&u=22a848503374fad225ab32b587b9083ca24c3a86&v=4", 
+        whatsapp:"662337766", 
+        bio:"10 anos de experiência em infra, atendimento rápido e satisafação garantida", 
+        subject:"Ajuda com Impressoras", 
+        cost: "25", 
+        weekday: [
+            0
+        ], 
+        time_from:[720], 
+        time_to : [1220]
+    },
+    {   name:"Henrique Rodrigues", 
+        avatar :"https://avatars1.githubusercontent.com/u/55166332?s=460&u=1fc9f988bb29e81c38322d51fd7f1501e558bd21&v=4", 
+        whatsapp:"662337766", 
+        bio:"Entusiasta das melhores tecnologias, tenho boas referências atuando como técnico em informática...", 
+        subject:"Ajuda com Software", 
+        cost: "80", 
+        weekday: [
+            0
+        ], 
+        time_from:[720], 
+        time_to : [1220]
+    }
+]
 
+const subjects = [
+    "Ajuda com Software",
+    "Ajuda com Hardware",
+    "Ajuda com Impressoras",
+    "Ajuda com Webcam",
+    "Formatação",
+    "Programação",
+    "Educação",
+]
+
+const weekdays = [
+    "Domingo",
+    "Segunda-feira",
+    "Terça-feira",
+    "Quarta-feira",
+    "Quinta-feira",
+    "Sexta-feira",
+    "Sábado",
+]
+
+
+function getSubject(subjectNumber){
+    const position = +subjectNumber - 1
+    return subjects[position] 
+
+}
+
+function pageLanding(req,res){
+    return res.render("index.html")
+
+}
+
+function pageStudy(req,res){
+    const filters = req.query
+    //console.log(req.query)
+    return res.render("study.html",{ proffys , filters ,subjects, weekdays})
+
+}
+
+
+
+function pageAlteracaodados(req,res){
+    const filters = req.query
+    return res.render("alteracaodados.html",{ proffys , filters ,subjects, weekdays})
+
+}
+
+function pageGiveClasses(req,res){
+    const data = req.query
+
+    const isNotEmpty = Object.keys(data).length > 0
+    
+
+    if(isNotEmpty){
+
+        data.subject = getSubject(data.subject)
+        proffys.push(data)
+        
+        return res.redirect("/study")
+        
+    }
+
+    return res.render("give-classes.html",{subjects, weekdays})
+
+}
 const express = require('express')
 const server = express()
-
-const { 
-    pageLanding, 
-    pageStudy,
-    pageAlteracaodados, 
-    pageGiveClasses, 
-    saveClasses 
-} = require('./pages')
-
 const nunjucks = require('nunjucks')
+
 //configurar nunjucks
+
 nunjucks.configure('src/views',{
     express: server,
     noCache: true,
     
 })
-
 
 server
 .use(express.static("public"))
@@ -25,6 +108,5 @@ server
 .get("/study",pageStudy)
 .get("/give-classes",pageGiveClasses)
 .get("/alteracaodados",pageAlteracaodados)
-.post("/save-classes", saveClasses)
 
 .listen(5000)
